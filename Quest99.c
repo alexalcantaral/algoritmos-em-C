1ebd1afaf42bf3420e44872093bfd77c7d8aef20
Alex Alcantara <alexalcantara280@gmail.com>
2024-01-24 19:03:11 -0300

    adicionando a lista das questoes

---
+++ Quest99.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <conio.h>
+
+const int quantidade = 100;
+
+void main(){
+
+    int k;
+
+    for(k=quantidade; k>=1; k--){
+        printf("%d\n", k);
+    }
+
+    getch();
+}
